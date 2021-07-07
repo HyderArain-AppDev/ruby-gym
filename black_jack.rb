3fcd73dd3ace9a51f7bc3a258b3560ae90e73fc5
hyderarain <harain@chicagobooth.edu>
2021-07-07 23:47:25 +0000

    did blackjack

--- black_jack.rb
+++ black_jack.rb
@@ -8,3 +8,22 @@
 # For example, given a 11 and 13 as input, the 11 should be 'converted' into a 1 so the total sum will be 14.
 
 p "Enter two number separated by spaces:"
+num_ar = gets.chomp.split(" ")
+num1 = num_ar[0].to_i 
+num2 = num_ar[1].to_i
+
+if num1 == 11 && num2 == 11
+    num1 = 1
+elsif num1 == 11
+    num1 = 1
+elsif num2 == 11
+    num2 = 1
+end
+
+sum = num1 + num2
+
+if sum > 21
+    sum = 0
+end
+
+puts sum
